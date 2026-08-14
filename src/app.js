8bacfa560e330745921f6a4c407f4b17e7da712c
notstanzinn <codeforces0608@gmail.com>
2026-08-14 15:16:06 +0530

    added user and video models

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express, { json } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+// cookie parser is used to store and modify cookies in the clients browser from server itself
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+})) // use method is mostly used for middlewares or configurations
+
+app.use(express.json({ limit : "16kb" }))
+app.use(express.urlencoded()) // to aceept data from url
+app.use(express.static("public")) // to store data in server only, like we have a public folder in this backend directory
+app.use(cookieParser())
+
+
+
+
+export { app }
---
+++ src/models/user.model.js
@@ -0,0 +1,85 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema({
+    username : {
+        type: String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        index : true,
+    },
+    email : {
+        type: String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+    },
+    fullname : {
+        type: String,
+        required : true,
+        trim : true,
+        index : true,
+    },
+    avatar : {
+        type: String, //cloudinary ka url use karenge
+        required : true,
+    },
+    coverImage : {
+        type : String, //cloudinary url
+    },
+    watchHistory : [
+        {
+            type: Schema.Types.ObjectId,
+            ref : "Video"
+        }
+    ],
+    password : {
+        type : String,
+        required : [true, "password is required"],
+    },
+    refreshToken : {
+        type : String,
+    }
+}, { timestamps : true })
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10) 
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    const isCorrect  = await bcrypt.compare(password, this.password)
+    return isCorrect
+}   
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )  //generates jwt token
+}
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )  //generates jwt token
+}
+
+export const User = mongoose.model("User", userSchema)
